2dc06ca13ec2c6eb7c933bb959efdb79fb176971
Samuel-DEVULDER <samuel.devulder@laposte.net>
2022-12-04 20:02:28 +0100

    added dhrystone source code

---
+++ 2022/ZPU/dhrystone/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export PATH="../gcc/bin:$PATH"
+zpu-elf-gcc -phi -DTIME dhry_1.c dhry_2.c -O3 -Wl,--relax -Wl,--gc-sections -o dhrystone.elf
+zpu-elf-size *.elf
+zpu-elf-objcopy -O binary dhrystone.elf dhrystone.bin
+cp dhrystone.bin ../roadshow/dhryston.bin
+cp dhrystone.bin ../build/
+#sh ../build/makefirmware.sh dhrystone.bin dhrystone.zpu
+
+
+
